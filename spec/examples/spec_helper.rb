27ba16d7dd5c37f483e10c1a11f97919bc81093c
John Crepezzi <john.crepezzi@patch.com>
2011-02-01 09:42:55 -0500

    Change how specs are written

--- spec/examples/spec_helper.rb
+++ spec/examples/spec_helper.rb
@@ -1,4 +1,2 @@
-require 'rubygems'
-require 'easy_translate'
-require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'locale_base')
-include LocaleBase
+require 'bundler/setup'
+require File.dirname(__FILE__) + '/../../lib/locale_base'
--- spec/examples/translate_task_spec.rb
+++ spec/examples/translate_task_spec.rb
@@ -4,54 +4,54 @@ require 'yaml'
 describe 'translate' do
 
   it 'should be able to convert a basic array' do
-    locale = Translator.new ['one', 'two', 'three']
+    locale = LocaleBase::Translator.new ['one', 'two', 'three']
     locale.translate(:to => :spanish).should == ['uno', 'dos', 'tres']
   end
 
   it 'should be able to convert a yaml file' do
-    locale = Translator.new YAML.load_file('spec/examples/sample/locale_basic.yml')
+    locale = LocaleBase::Translator.new YAML.load_file('spec/examples/sample/locale_basic.yml')
     lambda do
       locale.translate(:to => :spanish)
     end.should_not raise_error
   end
 
   it 'should be able to convert a hash with an array inside of it, and skip the keys' do
-    locale = Translator.new 'numbers' => ['one', 'two', 'three']
+    locale = LocaleBase::Translator.new 'numbers' => ['one', 'two', 'three']
     locale.translate(:to => :es).should == { 'numbers' => ['uno', 'dos', 'tres'] }
   end
 
   it 'should be able to convert a single string' do
-    locale = Translator.new 'uno'
+    locale = LocaleBase::Translator.new 'uno'
     locale.translate(:from => :es, :to => :en).should == 'one'
   end
 
   it 'should be able to have a complicated hash set' do
-    locale = Translator.new 'numbers' => { 'singles' => ['one', 'two'], 'teens' => ['ten', 'fifteen'] }
+    locale = LocaleBase::Translator.new 'numbers' => { 'singles' => ['one', 'two'], 'teens' => ['ten', 'fifteen'] }
     locale.translate(:from => :en, :to => :es).should == { 'numbers' => { 'singles' => ['uno', 'dos'], 'teens' => ['diez', 'quince'] } }
   end
 
   it 'should be able to work with {{variables}} without modifying them' do
-    locale = Translator.new 'one {{variable}} two'
+    locale = LocaleBase::Translator.new 'one {{variable}} two'
     locale.translate(:from => :en, :to => :es).should == 'uno {{variable}} dos'
   end
 
   it 'should be able to deal with multiple variables without confusing them' do
-    locale = Translator.new 'one {{var1}} two {{var2}} three'
+    locale = LocaleBase::Translator.new 'one {{var1}} two {{var2}} three'
     locale.translate(:from => :en, :to => :es).should == 'uno {{var1}} dos {{var2}} tres'
   end
 
   it 'should be able to deal with multiple variables with non-alphanum chars' do
-    locale = Translator.new 'one {{:var_1}} two {{:var_2}} three'
+    locale = LocaleBase::Translator.new 'one {{:var_1}} two {{:var_2}} three'
     locale.translate(:from => :en, :to => :es).should == 'uno {{:var_1}} dos {{:var_2}} tres'
   end
 
   it 'should be able to have two of the same variable in a string' do
-    locale = Translator.new 'one {{variable}} one {{variable}} two'
+    locale = LocaleBase::Translator.new 'one {{variable}} one {{variable}} two'
     locale.translate(:from => :en, :to => :es).should == 'uno {{variable}} uno {{variable}} dos'
   end
 
   it 'should be able to have two variables separated by a space' do
-    locale = Translator.new 'one {{variable1}} {{variable2}} two'
+    locale = LocaleBase::Translator.new 'one {{variable1}} {{variable2}} two'
     locale.translate(:from => :en, :to => :es).should == 'uno {{variable1}} {{variable2}} dos'
   end
 
@@ -64,14 +64,14 @@ describe 'translate' do
   it 'should be able to operate on extremely large objects, by chunking' do
     obj = []
     10.times { obj << "Now is the time for all good men to come to the aid of their country" }
-    locale = Translator.new obj
+    locale = LocaleBase::Translator.new obj
     translation = locale.translate(:from => :en, :to => :en)
     translation.size.should == 10
     translation.each { |t| t.should == 'Now is the time for all good men to come to the aid of their country' }
   end
 
   it 'should be able to translate something with a crazy evaluation inside of a variable spot' do
-    locale = Translator.new 'and this is {{puts 2*x + y}} math'
+    locale = LocaleBase::Translator.new 'and this is {{puts 2*x + y}} math'
     translation = locale.translate(:from => :en, :to => :es)
     translation.should == 'y esto es {{puts 2*x + y}} matemáticas'
   end
